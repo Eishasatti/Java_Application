d15adc79b744368821245fe28f088c1b41551b58
Eishasatti <esatti07@gmail.com>
2024-11-13 20:39:10 +0500

    classes and methods updated twice

---
+++ src/main/java/com/mycompany/my_hospital/My_Hospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.my_hospital;
+
+/**
+ *
+ * @author EISHA
+ */
+public class My_Hospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
